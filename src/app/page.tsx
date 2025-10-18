b11067606664c701bb43e7226c91a03b912c87eb
The.CraftsMan <ambroselington@gmail.com>
2025-10-18 02:04:56 +0100

    add image and reverse login page columns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ import { LoginForm } from '@/components/login-form';
 export default function LoginPage() {
   return (
     <div className='grid min-h-svh lg:grid-cols-2'>
-      <div className='flex flex-col gap-4 p-6 md:p-10'>
+      <div className='flex flex-col order-2 gap-4 p-6 md:p-10'>
         <div className='flex justify-center gap-2 md:justify-start'>
           <a href='#' className='flex items-center gap-2 font-medium'>
             <div className='bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md'>
               <GalleryVerticalEnd className='size-4' />
             </div>
-            Acme Inc.
+            Acme Inc..
           </a>
         </div>
         <div className='flex flex-1 items-center justify-center'>
@@ -20,7 +20,7 @@ export default function LoginPage() {
           </div>
         </div>
       </div>
-      <div className='bg-muted relative hidden lg:block'>
+      <div className='bg-muted order-1 relative hidden lg:block'>
         <img
           src='/sbts2.png'
           alt='Image'
